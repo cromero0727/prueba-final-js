54b678cd6aca0e19a9af9aab7ab599cd2cbc7d52
Cesar Romero <cromero@planok.com>
2021-03-30 01:22:32 -0300

    cargando archivos prueba final ja

--- src/assets/js/Serie.js
+++ src/assets/js/Serie.js
@@ -1,26 +1,18 @@
 class Serie {
-  constructor(nombre, personajes = []) {
-    this.nombre = nombre;
-    this.personajes = personajes;
-  }
-  // Debe inyectar html para renderizar la card de personaje
-  getPersonajes() {
-    const app = document.getElementById("app");
-    app.innerHTML = `
-    <div class="container">
-      <div class="row">
-        <div class="col-12">
-          
-        </div>
-      </div>
-    </div>
-    `
-  }
-  // this.personajes es una arreglo!
-  agregarPersonajes(personaje) {
-    // Si el parámetro es un objeto o un string
-    this.personajes.push(personaje);
-  }
+    constructor(nombre, personajes = []) {
+            this.nombre = nombre;
+            this.personajes = personajes;
+        }
+        // Debe inyectar html para renderizar la card de personaje
+    getPersonajes() {
+            const templateApp = document.getElementById("app");
+
+        }
+        // this.personajes es una arreglo!
+    agregarPersonajes(personaje) {
+        // Si el parámetro es un objeto o un string
+        this.personajes.push(personaje);
+    }
 }
 
 export default Serie;
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,50 +5,71 @@ import Serie from "./Serie";
 import Personaje from "./Personaje";
 
 const llamarPersonajes = (() => {
-  const urlBase = "https://rickandmortyapi.com/api";
-  // Variable privada que almacene la cantidad de personajes a mostrar.
-  let cantidadPersonajes = 0;
+    const urlBase = "https://rickandmortyapi.com/api";
+    // Variable privada que almacene la cantidad de personajes a mostrar.
+    let cantidadPersonajes = 0;
+    let serie = null;
 
-  const dataPersonajes = async (urlAPI, urlP) => {
-    let arr = [];
-    try {
-      const req = await fetch(urlAPI + urlP);
-      const data = await req.json();
-      arr = data.results;
-    } catch (error) {
-      console.log(
-        `Error en la obtención de los personajes desde la API: ${error}`
-      );
-    } finally {
-      return arr;
+
+    const dataPersonajes = async(urlAPI, urlP) => {
+        let arr = [];
+        try {
+            const req = await fetch(urlAPI + urlP);
+            const data = await req.json();
+            arr = data.results;
+        } catch (error) {
+            console.log(
+                `Error en la obtención de los personajes desde la API: ${error}`
+            );
+        } finally {
+            return arr;
+        }
+    };
+
+    const funcionPublica1 = async() => {
+        serie = new Serie("Rick and Morty");
+        // console.log(serie);
+
+        const resultadosAPI = await dataPersonajes(urlBase, "/character");
+        cantidadPersonajes = resultadosAPI.length;
+
+        resultadosAPI.forEach(personaje => {
+            // console.log(personaje);
+            const per = new Personaje(personaje.id, personaje.name, personaje.species, personaje.image);
+            serie.agregarPersonajes(per);
+        });
+    };
+    const funcionPublica2 = () => {
+        document.getElementById('numeroPersonajes').innerHTML = cantidadPersonajes;
+        document.getElementById('spinner').classList.remove('spinner-border')
+        const templateApp = document.getElementById("app");
+        serie.personajes.forEach(personaje => (
+            templateApp.innerHTML += `
+            
+                <div class="col-md-3 col-sm-6 col-xs-12">
+
+                    <div class="card mb-3" style="">
+                      <img src="${personaje.imagen}" class="card-img-top" alt="" id="imgPersonaje">
+                      <div class="card-body">
+                          <h5 class="card-title" id="nombrePersonaje">${personaje.obtenerNombre}</h5>
+                          <p class="card-text" id="especiePersonaje">${personaje.especie}</p>
+                      </div>
+                    </div>
+                    
+                  </div> 
+               
+          `
+        ));
+
     }
-  };
-
-  const funcionPublica1 = async() => {
-    const serie = new Serie("Rick and Morty");
-    
-    const resultadosAPI = await dataPersonajes(urlBase, "/character");
-    cantidadPersonajes = resultadosAPI.length;
-
-    resultadosAPI.forEach(personaje => {
-      const per = new Personaje(personaje.id, personaje.name, personaje.species, personaje.image);
-      serie.agregarPersonajes(per);
-    });
-  };
-  const funcionPublica2 = () => {
-    console.log(`Personajes a mostrar: ${cantidadPersonajes}`);
-    // setTimeout(() => {
-      // console.log(`Personajes a mostrar: ${cantidadPersonajes}`);
-    // }, 3000)
-  }
-  return {
-    funcionPublica1,
-    funcionPublica2,
-  };
+    return {
+        funcionPublica1,
+        funcionPublica2,
+    };
 })();
 
 llamarPersonajes.funcionPublica1();
 // llamarPersonajes.funcionPublica2();
 setTimeout(() => {
-  llamarPersonajes.funcionPublica2();
+    llamarPersonajes.funcionPublica2();
 }, 2000);
